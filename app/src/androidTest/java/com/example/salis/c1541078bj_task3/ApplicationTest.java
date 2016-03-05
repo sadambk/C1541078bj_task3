44a5faa39d56192f3253e020cc557675d78eeac8
sadambk <sadamaisha@yahoo.com>
2016-03-05 17:02:28 +0800

    Initial commit (interface)

---
+++ app/src/androidTest/java/com/example/salis/c1541078bj_task3/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.salis.c1541078bj_task3;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
