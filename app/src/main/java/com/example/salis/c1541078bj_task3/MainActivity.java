9052da9d86295c7dea137d5d467b6b9ffdc1bac9
sadambk <sadamaisha@yahoo.com>
2016-03-06 17:57:02 +0800

    1.Passing input data from editText on first application to textView of second application using intent. 2.Using startActivityForResult to get back result from the second application, and putting condition of only when the correct answer has been guess should result be sent back to the first application when the back button is pressed. 3.incrementing the score of the first application only when result retrieved back is correct. if back button is pressed without guessing the correct answer on the second application, then the score won't increment

--- app/src/main/java/com/example/salis/c1541078bj_task3/MainActivity.java
+++ app/src/main/java/com/example/salis/c1541078bj_task3/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.salis.c1541078bj_task3;
 
-
-
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,9 +10,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+    TextView scoreText;
+    EditText UserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +28,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        UserName = (EditText)findViewById(R.id.editText);
+        scoreText = (TextView)findViewById(R.id.textView4);
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,11 +53,33 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.Letter_Counting) {
 
             Intent LCount = new Intent(this, Main2Activity.class);
-            startActivity(LCount);
-            return (true);
+            LCount.putExtra("Username", UserName.getText().toString());
+            startActivityForResult(LCount, 1);
+
 
         }
 
         return super.onOptionsItemSelected(item);
     }
-}
+   public void onActivityResult(int requestCode, int resultCode, Intent data){
+
+
+
+
+        if(requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                ++counter;
+                scoreText.setText(String.valueOf(counter));
+            } else {
+                Toast.makeText(getApplicationContext(), "No Score to record!", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+        }
+
+
+
+    }
+
